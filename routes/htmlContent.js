8abc6156633a9c2a0e41642586287e9bdcdd7d59
Claudia <duquita96@gmail.com>
2024-02-13 22:32:27 +0100

    fixed post

--- routes/htmlContent.js
+++ routes/htmlContent.js
@@ -1,13 +1,10 @@
 import express from "express";
 
-
-
-const inputNotesSection = '<h1>add a new note</h1><input type="text" placeholder="Write here"><button>Add</button>';
-
-
+const inputNotesSection =
+  '<h1>add a new note</h1><input type="text" placeholder="Write here"><button>Add</button>';
 
 const htmlVariables = {
-    inputNotesSection
+  inputNotesSection,
 };
 
 export default htmlVariables;
